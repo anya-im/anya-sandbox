ada7896cab7b523b0f2191a6f272e72b4cdc5c61
Masahiko Hashimoto <hashimom@geeko.jp>
2023-01-25 07:39:06 +0900

    fixed converter

--- anyasand/converter.py
+++ anyasand/converter.py
@@ -9,47 +9,83 @@ from anyasand.dictionary import DictionaryConverter as Dictionary
 class Converter:
     def __init__(self, dnn_model, db_path):
         self._dict = Dictionary(db_path)
-        self._model = AnyaAE(self._dict.word_vec_size, self._dict.pos_len)
+        self._model = AnyaAE(self._dict.input_vec_size)
         self._model.load_state_dict(torch.load(dnn_model))
         self._criterion = nn.MSELoss(reduction='sum')
 
     def __call__(self, text):
-        in_vec = np.array([[self._dict.get(self._dict.wid_bos)]], dtype=np.float32)
-        y = self._model(torch.from_numpy(in_vec))
-
+        fixed = []
         word_tree = self._dict.build_word_tree(text)
-        for i, ym_ary in enumerate(word_tree):
-            print(ym_ary)
-            min_score = 1.
-            min_score_id = None
+        print(word_tree)
+
+        for crt_idx, ym_ary in enumerate(word_tree):
+            print(crt_idx, " -> ")
+            min_words = {"words": [], "cost": 0.}
+
             for ym in ym_ary:
-                words = self._dict.gets(ym)
-                for wid in words:
-                    score = self._score(in_vec, wid, y)
-                    """
-                    score = self._criterion(y, torch.from_numpy(self._dic.vector(word)).unsqueeze(0).unsqueeze(0))
-                    print(" %s: %f" % (word.decode().split("_")[0], score))
-                    if min_score > score:
-                        min_score = score
-                        min_score_id = word
-                    """
+                pre_idx = crt_idx - len(ym)
+                if pre_idx >= 0:
+                    for pre_ym in word_tree[pre_idx]:
+                        if pre_idx - len(pre_ym) < 0:
+                            for pre_word in self._dict.gets(pre_ym):
+                                words = [pre_word.decode()]
+                                in_vec = self._get_in_vec(words)
+                                pre_word_vec = self._model(in_vec)[0]
+                                predict_word_vec = self._model(in_vec)[1]
+                                vec = torch.from_numpy(self._dict.get(pre_word.decode()))
+                                pre_score = self._criterion(pre_word_vec, vec)
+                                pre_score = pre_score.item()
 
-                #print("  => %s" % min_score_id.decode().split("_")[0])
+                                for next_word in self._dict.gets(ym):
+                                    vec = torch.from_numpy(self._dict.get(next_word.decode()))
+                                    score = self._criterion(predict_word_vec, vec)
+                                    score = pre_score + score.item()
 
-                #idx, score = self._score(vec, cost)
-                #print(surfaces[idx], score)
-            break
+                                    copied = words.copy()
+                                    copied.append(next_word.decode())
 
-    def _score(self, in_vec, wid, y):
-        get_vec = self._dict.get(wid.decode())
-        in_vec_tmp = torch.from_numpy(get_vec)
-        #in_vec_tmp = np.vstack((in_vec, [[get_vec]]))
-        #y_tmp = self._model(torch.from_numpy(in_vec_tmp))
-        y_tmp = self._model(torch.from_numpy(in_vec))
-        score = self._criterion(y_tmp[0][0], in_vec_tmp)
-        diff = y_tmp[0][0] - in_vec_tmp
-        print(self._dict.wid2name(wid), score, diff)
-        return score
+                                    if min_words["cost"] == 0. or score < min_words["cost"]:
+                                        min_words["words"] = copied
+                                        min_words["cost"] = score
+
+                                        disp_words = []
+                                        for i, wid in enumerate(copied):
+                                            disp_words.append(self._dict.wid2name(wid))
+                                        print(" ",
+                                              disp_words,
+                                              min_words["cost"])
+
+                        else:
+                            words = fixed[pre_idx]["words"].copy()
+                            in_vec = self._get_in_vec(words)
+                            predict_word_vec = self._model(in_vec)[-1:]
+
+                            for next_word in self._dict.gets(ym):
+                                vec = torch.from_numpy(self._dict.get(next_word.decode()))
+                                score = self._criterion(predict_word_vec, vec)
+                                score = score.item() + fixed[pre_idx]["cost"]
+
+                                copied = words.copy()
+                                copied.append(next_word.decode())
+
+                                if min_words["cost"] == 0. or score < min_words["cost"]:
+                                    min_words["words"] = copied
+                                    min_words["cost"] = score
+
+                                    disp_words = []
+                                    for i, wid in enumerate(copied):
+                                        disp_words.append(self._dict.wid2name(wid))
+                                    print(" ",
+                                          disp_words,
+                                          min_words["cost"])
+
+            fixed.append(min_words)
+
+    def _get_in_vec(self, words):
+        in_vec = self._dict.get(self._dict.wid_bos)
+        for wid in words:
+            in_vec = np.vstack((in_vec, self._dict.get(wid)))
+        return torch.from_numpy(in_vec)
 
 
 def main():
